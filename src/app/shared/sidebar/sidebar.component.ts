aef23d09152158ad22c2b70b745e2839920c52d0
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-02-15 15:49:46 -0600

    se crea el servicio gifs.service.ts, se crea lista de istorial con ayuda de el servicio

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
-
+  
+  constructor(private gifsService: GifsService) {}
+  
+  get hist() {   
+    return this.gifsService.historial
+  }
 }
