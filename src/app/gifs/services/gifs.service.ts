f0220d6816190410d3c8daa0cc17102c78467707
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-02-23 12:59:09 -0600

    Petición http, uso de Api giphy y mostrar resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  private apiKey: string = '2Z6bAI4ZoHVLQS4Pnx2vTpUEbUVlMo1T';
   private _historial : string[] = [];
+
+  public resultado: any[] = [];
   
+  constructor(private http: HttpClient){}
+
   //Con este get puedo obtener una copia de los valores que haya en el historial.
   get historial() {
     return [...this._historial];
@@ -19,6 +26,9 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }    
 
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=2Z6bAI4ZoHVLQS4Pnx2vTpUEbUVlMo1T&q=${query}&limit=10`).subscribe((resp:any) => {
+      console.log(resp.data);
+      this.resultado = resp.data;
+    })
   }
 }
